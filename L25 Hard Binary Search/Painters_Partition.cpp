9a08ea9abc9b46d21ad46a700bafce9293e1a538
CoderCastor <tejasshinde935@gmail.com>
2024-02-23 11:46:52 +0530

    Painters_Partition Problem Solved

--- L25 Hard Binary Search/Painters_Partition.cpp
+++ L25 Hard Binary Search/Painters_Partition.cpp
@@ -33,9 +33,39 @@ i.e. at time = max(5+10, 30, 20+15) = 35
 #include<bits/stdc++.h>
 using namespace std;
 
+int findPages(int A[], int N, int M) 
+    {
+        //code here
+        if(M>N) return -1;
+        
+        int start=0,end=0,mid,ans;
+        for(int i=0;i<N;i++){
+            start=max(start,A[i]);
+            end+=A[i];
+        }
+        
+        while(start<=end){
+            mid=start+(end-start)/2;
+            
+            int pages = 0, count=1;
+            for(int i=0;i<N;i++){
+                pages+=A[i];
+                if(pages>mid){
+                    count++;
+                    pages=A[i];
+                }
+            }
+            if(count<=M){
+                ans=mid;
+                end=mid-1;
+            }
+            else start=mid+1;
+        }
+        return ans;
+        
+    }
+
 int main(){
-
-
-    
-    return 0;
+    int pages[5]={5,10,30,20,15};
+    cout<<"Ans : "<<findPages(pages,5,3);
 }
