072ec83085d35cee82175c92d6c165f279449bfd
CoderCastor <tejasshinde935@gmail.com>
2024-03-09 19:26:02 +0530

    recursion implementation of Tree added

---
+++ Tree/L2_1Recursion_Implementation.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *left,*right;
+    Node(int data){
+        this->data=data;
+        left=right=NULL;
+    }
+};
+
+//Function
+Node * BinaryTree(){
+    int x;
+    cin>>x;
+    if(x==-1) return NULL;
+
+    Node *temp = new Node(x);
+    //Left side Create
+    cout<<"Enter the left child of "<<x<<" : ";
+    temp->left = BinaryTree();
+    //Right side create
+    cout<<"Enter the right child of "<<x<<" : ";
+    temp->right = BinaryTree();
+    return temp;
+}
+
+int main(){
+    cout<<"Enter the root No. : ";
+    Node *root;
+    root = BinaryTree();
+}
